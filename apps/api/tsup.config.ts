d8e14fa3a4b78676504ac99eae639c7e492f2b84
Kushagra <kushagratrivedi962@gmail.com>
2026-05-27 22:29:26 +0530

    updated config

--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["./src/index.ts"],
   noExternal: [/(^@repo.*)/], // transpile packages starting with `@repo` and their dependencies
+  external: ["bcrypt", "mock-aws-s3", "aws-sdk", "nock", "@mapbox/node-pre-gyp"],
   splitting: false,
   bundle: true,
   outDir: "./dist",
